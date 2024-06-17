a893cf855ea9456f24c47974e0fa30ece5ce827d
yunji_choi <choiyj@kakao.com>
2024-06-17 21:50:03 +0900

    화면구성 작업중

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (<header>
+    <div className="logo">
+      <a href="#">
+        <img src="logo.png" alt="온누리" />
+      </a>
+    </div>
+  </header>)
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+export default function Nav() {
+  return (<nav className="bottom-nav">
+    <ul className="main-menu">
+      <li>
+        <a href="#">
+          <i class="fa-solid fa-circle-user"></i>
+          <span>MY</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa-solid fa-circle-user"></i>
+          <span>MY</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa-solid fa-circle-user"></i>
+          <span>MY</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa-solid fa-circle-user"></i>
+          <span>MY</span>
+        </a>
+      </li>
+    </ul>
+  </nav>)
+}
